b0456387da3bcd6f3ab9a3b1139a2de568527493
Evan White <evanwht1@gmail.com>
2022-04-10 14:09:55 -0600

    runnable java

--- java/src/main/java/com/stryd/workoutscheduler/WorkoutSchedulerApplication.java
+++ java/src/main/java/com/stryd/workoutscheduler/WorkoutSchedulerApplication.java
@@ -2,8 +2,12 @@ package com.stryd.workoutscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WorkoutSchedulerApplication {
 
 	public static void main(String[] args) {
@@ -11,9 +15,9 @@ public class WorkoutSchedulerApplication {
 	}
 
 
-	@GetMapping("/")
-	public String Hello() {
-		return "<p>Hello, World!</p>";
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 	// Add your new REST endpoint(s) here
